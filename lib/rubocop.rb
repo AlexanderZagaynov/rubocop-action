631ee52645ff1b22849f50ed3e43f9d24d078792
Alexander Zagaynov <zalex80@gmail.com>
2019-11-02 16:59:07 +0100

    better ls and more info

--- lib/rubocop.rb
+++ lib/rubocop.rb
@@ -2,7 +2,8 @@
 
 require 'json'
 
-puts 'ARGV:', JSON.pretty_generate(ARGV)
-puts 'ENV:',  JSON.pretty_generate(ENV.to_h)
-puts 'pwd:',  `pwd`
-puts 'ls:',   `ls -lah ../**/`
+puts 'ARGV:',  JSON.pretty_generate(ARGV)
+puts 'ENV:',   JSON.pretty_generate(ENV.to_h)
+puts 'pwd:',   `pwd`
+puts 'ls:',    `cd ..; ls -lah **; cd -`
+puts 'event:', `cat /github/workflow/event.json`
